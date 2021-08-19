fb35da292c8c382d109f481dbb96495e42a67d82
t1dmlgus <dmlgusgngl@gmail.com>
2021-08-19 09:30:15 +0900

    feat: 백엔드 init

---
+++ src/test/java/com/s1dmlgus/JpaBook/JpaBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1dmlgus.JpaBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
